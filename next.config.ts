548a0f679be243943f8b3674bd56b2f08d0a4734
oyindamola Babatola <oyindamola2383@gmail.com>
2025-03-18 16:29:32 +0100

    pwa

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,39 @@
+// import { WithPWA } from "next-pwa";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["your-image-host.com"],
+//     deviceSizes: [320, 420, 768, 1024, 1200],
+//     imageSizes: [16, 32, 48, 64, 96],
+//   },
+// };
+
+// export default WithPWA({
+//   ...nextConfig, 
+//   pwa: {
+//     dest: "public",
+//     register: true,
+//     skipWaiting: true,
+//   },
+// });
+import withPWA from "next-pwa"; // ✅ Correct import
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // images: {
-  //   domains: ['your-image-host.com'],
-  //   deviceSizes: [320, 420, 768, 1024, 1200],
-  //   imageSizes: [16, 32, 48, 64, 96],
-  // },
-
+  reactStrictMode: true,
+  images: {
+    domains: ["your-image-host.com"],
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [16, 32, 48, 64, 96],
+  },
 };
 
-export default nextConfig;
+export default withPWA({
+  ...nextConfig, 
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+
+});
